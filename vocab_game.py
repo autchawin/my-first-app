46b724ac43df9d9d19a706705d3ab012aff3aea4
402-18-Autchawin <aukchawin.sirisuchakul@gmail.com>
2026-08-19 15:31:41 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -124,7 +124,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นาย อัชวิน สริสุชากุล เลขที่ 18 ม.4/2")

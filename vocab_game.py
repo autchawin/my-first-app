d894e7c645cb8b7993c93959afef18a2035936f1
402-18-Autchawin <aukchawin.sirisuchakul@gmail.com>
2026-08-19 14:56:09 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,4 +101,4 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นาย อัชวิน สริสุชากุล เลขที่ 18 ม.4/2")

0905a84638a299cfb23d96431b52a2ac44c019a7
402-18-Autchawin <aukchawin.sirisuchakul@gmail.com>
2026-08-26 15:14:52 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -51,13 +51,13 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-    if u_ans == "laptop":
+    if u_ans3 == "laptop":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
-    if u_ans == "banana":
+    if u_ans4 == "banana":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
